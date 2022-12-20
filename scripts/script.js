238e57c1374ed188c4b305ac93230c48c0a5db6b
Matsonj30 <matsonj2013@gmail.com>
2022-12-19 22:09:53 -0700

    testing

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 function burgerMenu(){
     var menu = document.getElementById("menu");
     if(menu.clientHeight == 0){
-        menu.style.animation = "mobile-open-menu 0.4s linear";
+        menu.style.animation = "mobile-open-menu 0.4s ease-in";
     }
    
     else{
-        menu.style.animation = "mobile-close-menu 0.4s linear";
+        menu.style.animation = "mobile-close-menu 0.4s ease-in";
     }
 }
